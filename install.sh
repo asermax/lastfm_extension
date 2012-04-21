f7605931457ca6066e873fa5ac9a85fd78344eae
asermax <kiira@mint>
2012-04-21 17:28:41 -0300

    Fixed the install script.

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 GLIB_SCHEME="org.gnome.rhythmbox.plugins.lastfm_extension.gschema.xml"
-GLIB_DIR="home/${USER}/.local/share/glib-2.0/schemas"
-SCRIPT_NAME=`basename $0`
-PLUGIN_PATH="/home/${USER}/.local/share/rhythmbox/plugins/lastfm_extension"  
+GLIB_DIR="/home/${USER}/.local/share/glib-2.0/schemas/"
+SCRIPT_NAME=`basename "$0"`
+SCRIPT_PATH=${0%`basename "$0"`}
+PLUGIN_PATH="/home/${USER}/.local/share/rhythmbox/plugins/lastfm_extension/"  
 
-#build the dir
-mkdir $PLUGIN_PATH
+#build the dirs
+mkdir -p $PLUGIN_PATH
+mkdir -p $GLIB_DIR
 
 #copy the files
-cp "${0}/* $PLUGIN_PATH"
+cp -r "${SCRIPT_PATH}"* "$PLUGIN_PATH"
 
 #install the glib schema
-mv "${PLUGIN_PATH}/${GLIB_SCHEME} $GLIB_DIR"
-glib-compile-schemas $GLIB_DIR
+mv "${PLUGIN_PATH}${GLIB_SCHEME}" "$GLIB_DIR"
+glib-compile-schemas "$GLIB_DIR"
 
 #remove the install script from the dir (not needed)
-rm "${PLUGIN_PATH}/${SCRIPT_NAME}"
+rm "${PLUGIN_PATH}${SCRIPT_NAME}"
